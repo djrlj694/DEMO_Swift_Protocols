e3768343af3793ae0325b872d552f78ecb3bd597
Robert L. Jones <robert.l.jones@synthelytics.com>
2016-02-04 15:11:10 -0500

    Created folder structure for source code files.  Move 2 source code files (AppDelegate.swift, ViewController.swift) accordingly.

--- Demo 1.playground/Contents.swift
+++ Demo 1.playground/Contents.swift
@@ -1,5 +1,47 @@
 //: Playground - noun: a place where people can play
 
-import Cocoa
+import UIKit
+import HealthKit
 
-var str = "Hello, playground"
+protocol Measurable
+{
+    /*
+    var unit: HKUnit { get }
+    var quantity: HKQuantity { get }
+    var quantityType: UnitSystem { get }
+    */
+    var measurements: Set<Measurement> { get }
+}
+
+// SOURCE: https://en.wikipedia.org/wiki/Units_of_measurement#Metric_systems
+
+enum UnitSystem
+{
+    case English
+    case SI
+    case Other
+}
+
+// SOURCE: https://en.wikipedia.org/wiki/SI_base_unit
+
+enum UnitType
+{
+    case AmountOfSubstance     // SI base unit
+    case ElectricCurrent       // SI base unit
+    case ElectricalConductance // HealthKit unit
+    case Energy                // HealthKit unit
+    case Length                // HealthKit unit, SI base unit
+    case LuminousIntensity     // SI base unit
+    case Mass                  // HealthKit unit, SI base unit
+    case Pressure              // HealthKit unit
+    case Side                  // Geometric unit
+    case Temperature           // HealthKit unit, SI base unit
+    case Time                  // HealthKit unit, SI base unit
+    case Volume                // HealthKit unit
+}
+
+struct Measurement, Hashable {
+    var unit: HKUnit
+    var quantity: HKQuantity
+    var quantityType: UnitSystem
+}
