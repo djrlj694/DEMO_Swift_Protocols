7c01c777f969aaaacff26f50798e1c75cde87fd9
Robert L. Jones <robert.l.jones@synthelytics.com>
2016-02-05 18:57:58 -0500

    Updated Polygon class to conform to HasSides protocol.

--- DEMO_Swift_Protocols/Sources/Models/Polygon.swift
+++ DEMO_Swift_Protocols/Sources/Models/Polygon.swift
@@ -8,16 +8,20 @@
 
 import Foundation
 
-class Polygon: Shape {
+class Polygon: Shape, HasSides {
     
-    init(area: Area) {
+    var sides: Sides
+    
+    init(area: Area, sides: Sides) {
+        self.sides = sides
+        
         super.init(type: .Polygon, area: area)
     }
     
-    convenience init(side: Length, numberOfSides: Sides) {
+    convenience init(side: Length, sides: Sides) {
         let π = 3.1415
-        let area = Area(unit: side.unit, value: π*numberOfSides.value*pow(side.value, 2)/(4*tan(π/numberOfSides.value)))
+        let area = Area(unit: side.unit, value: π*sides.value*pow(side.value, 2)/(4*tan(π/sides.value)))
         
-        self.init(area: area)
+        self.init(area: area, sides: sides)
     }
 }
