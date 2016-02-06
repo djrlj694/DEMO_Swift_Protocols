1c0d95e6b3eab3176ddf51ae6abfd3d03f78c9f0
Robert L. Jones <robert.l.jones@synthelytics.com>
2016-02-05 21:00:43 -0500

    Deleted NorthAmerica and Europe class as they would be instances of a class called Continent.  Created Continent class.

--- DEMO_Swift_Protocols/Sources/Models/Continent.swift
+++ DEMO_Swift_Protocols/Sources/Models/Continent.swift
@@ -1,5 +1,5 @@
 //
-//  Europe.swift
+//  Continent.swift
 //  DEMO_Swift_Protocols
 //
 //  Created by Robert L. Jones on 2/5/16.
@@ -8,16 +8,10 @@
 
 import Foundation
 
-class Europe: Geography {
+class Continent: Geography {
     
     init(area: Area, population: Population) {
         super.init(type: .Continent, area: area, population: population)
     }
     
-    convenience init(radius: Length) {
-        let area = Area(unit: "km^2", value: 10180000)
-        let population = Population(unit: "person", value: 742452000)
-        
-        self.init(area: area, population: population)
-    }
 }
